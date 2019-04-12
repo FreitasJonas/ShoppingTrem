1877b6b08c80a1a1e906741c10da1e0ce03d713a
FreitasJonas <jonascontato15@gmail.com>
2019-04-12 12:55:34 -0300

    game menu

---
+++ Assets/Scripts/Scenes/menu/MenuSceneController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MenuSceneController : MonoBehaviour
+{
+    private string GameScene = "Scene_01";
+
+    public void playAudio(GameObject audioSource)
+    {
+        Instantiate(audioSource);
+    }
+
+    public void GoToGameScene()
+    {
+        Initiate.Fade(GameScene, Color.black, 0.5f);
+    }
+}
---
+++ Assets/Scripts/UIElements/Songs/audioBtnPressLimit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class audioBtnPressLimit : MonoBehaviour
+{
+    private float limit = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        limit -= Time.deltaTime;
+
+        if (limit <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
